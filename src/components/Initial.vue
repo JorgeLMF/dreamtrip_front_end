06f16d1d303e525afe40f57d7437afaed4d68d39
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2022-09-20 20:12:03 -0500

    arreglo de botones

--- src/components/Initial.vue
+++ src/components/Initial.vue
@@ -12,15 +12,21 @@ const count = ref(0)
   <div class="title-initial">DreamTrip</div>
 
   <div class="slogan">Create your dream trip</div>
+  
+  <div class="space-1"> 
+    <button class="initial-button-sign-up">Sign up</button>
+  </div>
+  
+  <div class="space-2"> 
+    <button class="initial-button-log-in">Log in</button>
+  </div>
 
-  <button class="initial-button-sign-up" >Sign up</button>
-
-  <button class="initial-button-log-in" >Log in</button>
 </template>
 
 <style scoped>
   .title-initial{
     font-size: 96px;
+    color: #ffffff;
     margin: 60px;
     
   }
@@ -32,24 +38,28 @@ const count = ref(0)
     margin: 40px;
   }
 
-  .initial-buttom-sign-up{
+  .initial-button-sign-up{
     background-color: #FC4747;
-    margin-bottom: 50px;
-    
-    padding-left: 50px;
-    padding-right: 50px;
+    padding-left: 100px;
+    padding-right: 100px;
     border: 0px;
-    border-radius: 5px;
+    border-radius: 10px;
   }
 
-  .initial-buttom-log-in{
+  .initial-button-log-in{
     background-color: #FC4747;
-    margin-bottom: 50px;
-    
-    padding-left: 50px;
-    padding-right: 50px;
+    padding-left: 105px;
+    padding-right: 105px;
     border: 0px;
-    border-radius: 5px;
+    border-radius: 10px;
+  }
+
+  .space-1{
+    margin-top: 80px;
+  }
+
+  .space-2{
+    margin: 20px;
   }
 
 </style>
