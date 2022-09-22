cb86500bae0db990620fb4330363795bd48e5b46
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2022-09-22 17:08:49 -0500

    update sign up page

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,95 +1,98 @@
 <template>
-
-  <div class="singup">
-    <div class="space-1">
-      <div class="title-sig">Sign Up</div>
+  <div class="signup">
+    <div>
+      <div class="title-log">Signup</div>
     </div>
-    
-    <div class="space-2">
-      <form action="/action_page.php">
-        <input type="text" value="Email address" class="input"><br><br>
-        <input type="text" value="Password" class="input"><br><br>
-        <input type="text" value="Repeat password" class="input"><br><br>
+
+    <div>
+      <form method="get" id="form-signup">
+        <input
+          type="text"
+          placeholder="Email address"
+          class="input"
+        /><br /><br />
+        <input type="text" placeholder="Password" class="input" /><br /><br />
+        <input
+          type="text"
+          placeholder="Repeat Password"
+          class="input"
+        /><br /><br />
       </form>
     </div>
-    
-    <div class="space-3">
-      <router-link to="/login">
-        <button class="initial-button-log-in">Create an account</button>
+
+    <div>
+      <router-link to="/home">
+        <button type="submit" form="form-signup" class="initial-button-log-in">
+          Create an Account
+        </button>
       </router-link>
     </div>
-    
-    <div class="space-4">
-      <div>
-        Already have an account? <router-link to="/login" class="link">Login</router-link>  
-      </div>
+
+    <div class="signup-to-signup">
+      Already have an account?
+      <router-link to="/signup" class="link">Login</router-link>
     </div>
-            
-
   </div>
-
-
-    
 </template>
-    
+
 <style scoped>
-      
-      .singup{
-        background-color: #161D2F;
-        margin: auto;
-        border-radius: 20px;
-        display: flex;
-        flex-direction: column;
-        width: 120%;
-        height: 110%;
-      }
+.signup {
+  background-color: #161d2f;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  row-gap: 25px;
+  padding: 32px;
+  justify-content: space-between;
+}
 
-      .title-sig{
-        color: #ffffff;
-        font-size: 32px;
-        text-align: left;
-        padding-left: 20px;
-        padding-top: 30px;
-      }
+#form-signup {
+  padding-top: 20px;
+}
 
-      .input{
-        border: 0px;
-        background-color: #161D2F;
-        text-align: left;
-        padding-left: 20px;
-        padding-bottom: 10px;
-        border-bottom: 1px solid #5A698F;
+.title-log {
+  color: #ffffff;
+  font-size: 32px;
+  text-align: left;
+}
 
-      }
+.input {
+  border: 0px;
+  background-color: #161d2f;
+  text-align: left;
+  border-bottom: 1px solid #5a698f;
+  width: 100%;
+  padding-bottom: 13px;
+}
 
-      .initial-button-log-in{
-        background-color: #FC4747;
-        padding-left: 40px;
-        padding-right: 40px;
-        border: 0px;
-        border-radius: 10px;
-      }
+#form-signup input:focus {
+  outline: none;
+}
 
-      .link{
-        color:#FC4747;
-      }
-  
-      .space-1{
-        margin-bottom: 60px;
-      }
-    
-      .space-2{
-        margin: 5px;
+.input::placeholder {
+  color: #ffffff;
+  opacity: 0.5;
+}
 
-      }
-      
-      .space-3{
-        margin: 5px;
-      }
+.initial-button-log-in {
+  background-color: #fc4747;
+  padding-left: 50px;
+  padding-right: 50px;
+  border: 0px;
+  border-radius: 10px;
+  width: 336px;
+  height: 48px;
+  font-size: 15px;
+  font-weight: 300;
+}
 
-      .space-4{
-        margin: 5px;
-        padding-bottom: 20px;
-      }
-    
+.link {
+  color: #fc4747;
+}
+
+.signup-to-signup {
+  font-size: 15px;
+  font-weight: 300;
+}
 </style>
